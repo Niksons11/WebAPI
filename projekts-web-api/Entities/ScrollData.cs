5fc5963ac8285dbdf61a28832e614820b565b43a
Niksons11 <niks.ritogy@gmail.com>
2020-06-14 11:57:57 +0300

    infinite scroll WIP

---
+++ projekts-web-api/Entities/ScrollData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Entities
+{
+    public class ScrollData
+    {
+        // Record if the next page is requested.
+        public string paging { get; set; }
+    }
+}
---
+++ projekts-web-api/Models/ResultsPerPageModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class ResultsPerPageModel
+    {
+        public static class GlobalVariables
+        {
+            public static int ResultsPerPage
+            {
+                get
+                {
+                    return 3;
+                }
+            }
+        }
+    }
+}
